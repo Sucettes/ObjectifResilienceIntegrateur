30585b329b9c87862225b24806d6b8dbd7d4bbaa
rosemariegrondin <rose-marie.grondin@live.ca>
2022-02-10 10:35:31 -0500

    include wwwroot files

---
+++ src/Web/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+(function () {
+'use strict';
+
+// import 'babel-polyfill';
+// import { Validation as validation } from './modules/input-validation'
+// (() => {
+//  setTimeout(() => {
+//  validation.init();
+// }, 50);
+// })();
+
+}());
