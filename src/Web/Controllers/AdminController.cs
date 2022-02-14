8eb4ab345609214cd4a0b7895f3c03be0a14e678
WaterWalo <57630182+WaterWalo@users.noreply.github.com>
2022-02-14 14:26:56 -0500

    Commit Controller Pour Antoine

---
+++ src/Web/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gwenael.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
