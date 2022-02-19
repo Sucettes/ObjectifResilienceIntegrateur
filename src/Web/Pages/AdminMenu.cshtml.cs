39db80daa6cc71f2aa660eb2ce8b6f110753db59
WaterWalo <57630182+WaterWalo@users.noreply.github.com>
2022-02-19 13:45:16 -0500

    Ajout de la modification/ consultation / des Utilisateurs

---
+++ src/Web/Pages/AdminMenu.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Gwenael.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Gwenael.Domain;
+
+namespace Gwenael.Web.Pages
+{
+    public class AdminMenuModel : PageModel
+    {
+        private readonly GwenaelDbContext _context;
+
+        public AdminMenuModel(GwenaelDbContext context)
+        {
+            _context = context;
+            // Ajout Manuel d'un user
+            //User antho = new User
+            //{
+            //    Active = true,
+            //    FirstName = "Anthony",
+            //    LastName = "Levesque",
+            //    Email = "antho20k@hotmail.com"
+            //};
+            //_context.Add<User>(antho);
+            //_context.SaveChanges();
+        }
+        public IList<User> Users { get; set; }
+        public async Task OnGetAsync()
+        {
+            Users = await _context.Users.ToListAsync();
+            ViewData["lstUsers"] = Users;
+        }
+        public void OnPost() {
+            
+        }
+
+    }
+}
---
+++ src/Web/Pages/User.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Gwenael.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Gwenael.Domain;
+
+namespace Gwenael.Web.Pages
+{
+    public class UserModel : PageModel
+    {
+        private readonly GwenaelDbContext _context;
+
+        public UserModel(GwenaelDbContext context)
+        {
+            _context = context;
+        }
+        [BindProperty(SupportsGet = true)]
+        public User user { get; set; }
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            user = await _context.Users.FindAsync(id);
+            ViewData["user"] = user;
+            return Page();
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            //if (ModelState.IsValid)
+            //{
+            var userBd = await _context.Users.FindAsync(user.Id);
+            userBd.UserName = user.UserName;
+            userBd.Email = user.Email;
+            userBd.LastName = user.LastName;
+            userBd.FirstName = user.FirstName;
+
+            await _context.SaveChangesAsync();
+            return RedirectToPage("adminMenu");
+
+            //}
+        }
+
+    }
+}
