f3374efe4384b1cceaa64bb36cde0e5760524b38
WaterWalo <57630182+WaterWalo@users.noreply.github.com>
2022-02-23 12:14:52 -0500

    Ajout Formation (CRUD) / Panel avec onglet

---
+++ src/Domain/Entities/Formation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gwenael.Domain.Entities
+{
+    public class Formation
+    {
+        public int Id { get; set; }
+        [Required][MaxLength(50)] public string Name { get; set; }
+        [MaxLength(50)] public string Description { get; set; }
+
+    }
+}
