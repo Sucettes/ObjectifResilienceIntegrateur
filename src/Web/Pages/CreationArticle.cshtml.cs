8f312e1f437754bba7f0af61f040bf09d31642ad
Gwenael Florian Galliot <g.f.galliot@edu.cegepgarneau.ca>
2022-02-28 14:38:33 -0500

    tst nouvelle branche

---
+++ src/Web/Pages/CreationArticle.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gwenael.Web.Pages
+{
+    public class CreationArticle : PageModel
+    {
+        public string PersonalDetails { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostSubmit(string personalDetails)
+        {
+            this.PersonalDetails = personalDetails;
+            
+        }
+    }
+}
