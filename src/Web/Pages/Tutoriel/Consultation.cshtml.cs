f80198ff2b3ac78029b02cb776ea40882da92d8a
Antoine Bédard <79115434+antoinebedard2002@users.noreply.github.com>
2022-03-28 14:30:47 -0400

    Correction version AWS + correction bug tuto

--- src/Web/Pages/Tutoriel/Consultation.cshtml.cs
+++ src/Web/Pages/Tutoriel/Consultation.cshtml.cs
@@ -1,26 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Data;
-using System.Globalization;
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Text;
-using System.Reflection;
-using System.Threading.Tasks;
 using System.Linq;
-using CsvHelper;
-using CsvHelper.Configuration;
 using Gwenael.Domain;
 using Gwenael.Domain.Entities;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using NuGet.Packaging;
-using NuGet.Protocol;
 using Spk.Common.Helpers.String;
 
 
