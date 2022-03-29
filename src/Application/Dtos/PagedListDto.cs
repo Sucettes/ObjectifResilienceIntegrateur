d6741ccb36279e7c5d1204707a274f494f1bb639
WaterWalo <57630182+WaterWalo@users.noreply.github.com>
2022-03-29 13:53:03 -0400

    Comment Test

--- src/Application/Dtos/PagedListDto.cs
+++ src/Application/Dtos/PagedListDto.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections.Generic;
+﻿//using System.Collections.Generic;
 
-namespace Gwenael.Application.Dtos
-{
-    public class PagedListDto<T>
-    {
-        public int CurrentPage { get; set; }
-        public int PageSize { get; set; }
-        public int TotalItems { get; set; }
-        public int TotalPages { get; set; }
-        public IEnumerable<T> Items { get; set; }
+//namespace Gwenael.Application.Dtos
+//{
+//    public class PagedListDto<T>
+//    {
+//        public int CurrentPage { get; set; }
+//        public int PageSize { get; set; }
+//        public int TotalItems { get; set; }
+//        public int TotalPages { get; set; }
+//        public IEnumerable<T> Items { get; set; }
 
-        public PagedListDto()
-        {
-        }
-    }
-}
+//        public PagedListDto()
+//        {
+//        }
+//    }
+//}
