6633d4693d50267bf378422909e27798edeaa1f3
WaterWalo <57630182+WaterWalo@users.noreply.github.com>
2022-03-29 14:07:12 -0400

    Comment PostMan  and test

--- src/Application/Dtos/ResetPasswordDto.cs
+++ src/Application/Dtos/ResetPasswordDto.cs
@@ -1,10 +1,10 @@
-﻿//namespace Gwenael.Application.Dtos
-//{
-//    public class ResetPasswordDto
-//    {
-//        public string Id { get; set; }
-//        public string UserName { get; set; }
-//        public string Code { get; set; }
-//        public string NewPassword { get; set; }
-//    }
-//}
+﻿namespace Gwenael.Application.Dtos
+{
+    public class ResetPasswordDto
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Code { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
