f38652364283bc8bda7fe6ad6b9c443bd936a599
Antoine Bédard <79115434+antoinebedard2002@users.noreply.github.com>
2022-03-29 20:19:11 -0400

    Modification emplacement des scripts fichier javascript

---
+++ src/Web/wwwroot/javascript/tutorielSupprimer.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    'use strict';
+
+    var idTuto = "";
+
+    $('[data-supprimerTuto]').on('click', event => {
+        event.preventDefault();
+        event.stopPropagation();
+        let boutton = event.currentTarget;
+
+        idTuto = boutton.form[0].value
+    });
+
+
+
+    $('#btnConfirmDeleteTuto').click(function () {
+        var href = window.location.pathname + "?handler=DeleteTuto";
+
+        var tutoVal = { 'tutorielIdVal': idTuto }
+
+        $.ajax({
+            type: 'POST',
+            url: href,
+            data: JSON.stringify(tutoVal),
+            contentType: "application/json",
+            headers: {
+                RequestVerificationToken:
+                    $('input:hidden[name="__RequestVerificationToken"]').val()
+            },
+            success: function () {
+                //window.location.replace(window.location + "&deleteStatus=true");
+                //window.location.replace(window.location);
+                window.location.replace('/Tutoriel?deleteStatus=true');
+
+
+            },
+            error: function () {
+                //window.location.replace(window.location + "&deleteStatus=false");
+                //window.location.replace(window.location);
+                window.location.replace('/Tutoriel?deleteStatus=false');
+
+
+            }
+        });
+    });
+
+}());
