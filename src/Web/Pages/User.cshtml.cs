f3bef51d137db3d9482244b835251c60f3143236
WaterWalo <57630182+WaterWalo@users.noreply.github.com>
2022-04-04 12:35:39 -0400

    Update User / Ajout des mot de passe Hashé via le panneau admin

--- src/Web/Pages/User.cshtml.cs
+++ src/Web/Pages/User.cshtml.cs
@@ -6,16 +6,21 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
 using Gwenael.Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Gwenael.Web.Pages
 {
     public class UserModel : PageModel
     {
         private readonly GwenaelDbContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly PasswordHasher<User> _passHasher = new PasswordHasher<User>();
 
-        public UserModel(GwenaelDbContext context)
+
+        public UserModel(GwenaelDbContext context, UserManager<User> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         [BindProperty(SupportsGet = true)]
         public User user { get; set; }
@@ -23,17 +28,18 @@ namespace Gwenael.Web.Pages
         {
             if (id.ToString() is null or "")
             {
-                return Page();
+                return RedirectToPage("User");
             }
             else
             {
                 user = await _context.Users.FindAsync(id);
                 ViewData["user"] = user;
+                ViewData["passwordUser"] = user.PasswordHash;
                 return Page();
             }
 
         }
-        public async Task<IActionResult> OnPost(string btnSave, string btnDelete, string btnAdd)
+        public async Task<IActionResult> OnPost(string btnSave, string btnDelete, string btnAdd, string NewPassword)
         {
             User userBd = null;
             try
@@ -49,27 +55,38 @@ namespace Gwenael.Web.Pages
                 userBd.Email = user.Email;
                 userBd.LastName = user.LastName;
                 userBd.FirstName = user.FirstName;
+                if (NewPassword is not null)
+                {
+                    string hashedPassword = _passHasher.HashPassword(userBd, NewPassword);
+                    userBd.PasswordHash = hashedPassword;
 
+                }
                 await _context.SaveChangesAsync();
                 return RedirectToPage("adminMenu");
             }
             else if (btnDelete != null)
             {
                 _context.Users.Remove(userBd);
-                await _context.SaveChangesAsync();
+                _context.SaveChanges();
                 return RedirectToPage("adminMenu");
             }
             else if (btnAdd != null)
             {
-                //Ajout Manuel d'un user
+                if (NewPassword is not null)
+                {
+                    string hashedPassword = _passHasher.HashPassword(userBd, NewPassword);
+                    user.PasswordHash = hashedPassword;
+                }
                 User newUser = new User
                 {
                     FirstName = user.FirstName,
                     LastName = user.LastName,
                     Email = user.Email,
-                    UserName = user.UserName
+                    UserName = user.UserName,
+                    PasswordHash = user.PasswordHash
+
                 };
-                _context.Add<User>(newUser);
+                _context.Users.Add(newUser);
                 _context.SaveChanges();
                 return RedirectToPage("adminMenu");
             }
