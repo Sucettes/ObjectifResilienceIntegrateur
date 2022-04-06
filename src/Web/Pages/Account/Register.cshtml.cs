0069b0c1ba0a5528c1817bba66b202afcf585e6d
WaterWalo <57630182+WaterWalo@users.noreply.github.com>
2022-04-06 09:36:40 -0400

    Register Fonctionnelle

--- src/Web/Pages/Account/Register.cshtml.cs
+++ src/Web/Pages/Account/Register.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Logging;
 using Gwenael.Application.Mailing;
 using Gwenael.Domain.Entities;
 using Gwenael.Web.Extensions;
+using System;
 
 namespace Gwenael.Web.Pages.Account
 {
@@ -63,8 +64,9 @@ namespace Gwenael.Web.Pages.Account
             ReturnUrl = returnUrl;
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                User user = new User { UserName = Input.Email, Email = Input.Email, FirstName = "", LastName = "" };
                 var result = await _userManager.CreateAsync(user, Input.Password);
+                Console.WriteLine(result);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
@@ -73,7 +75,6 @@ namespace Gwenael.Web.Pages.Account
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     await _emailFactory.SendEmailConfirmationAsync(Input.Email, callbackUrl);
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
                     return LocalRedirect(Url.GetLocalUrl(returnUrl));
                 }
                 foreach (var error in result.Errors)
